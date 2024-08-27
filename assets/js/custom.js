d217c4e82b6b13d3e6e2819f2840bc681b858672
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-08-27 14:14:24 +0700

    update file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,6 +36,50 @@ jQuery(function($) {
 			scrollTop: 0
 		}, 500)
 	});
+
+	//slick slider
+	
+	$('.hero-bg').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
+		arrows: false,
+		dots: false,
+		autoplay: false,
+		autoplaySpeed: 2000,
+		variableWidth:true,
+		centerMode: true,
+		cssEase: 'linear',
+		responsive: [{ 
+			breakpoint: 500,
+			settings: {
+				dots: false,
+				arrows: false,
+				infinite: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				draggable: true,
+			} 
+		}]
+	});
+
+	// show/hide slider content
+	$(document).ready(function() {
+		var panes = $('.hero-text li');
+	
+		function updateTextContent(index) {
+			panes.removeClass('active');
+			panes.eq(index).addClass('active');
+		}
+	
+		$('.hero-bg').on('afterChange', function(event, slick, currentSlide) {
+			updateTextContent(currentSlide);
+		});
+	
+		updateTextContent(0);
+	});
+	
 });
 
 
